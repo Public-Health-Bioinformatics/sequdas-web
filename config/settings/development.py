9383836d76499330e177a702666fdf9bcbfafff9
Dan Fornika <dfornika@gmail.com>
2019-01-07 13:49:19 -0800

    Protected views

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,3 +1,4 @@
 from .base import *
 
 MIDDLEWARE.append('config.middleware.dev_cors_middleware')
+REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'].append('rest_framework.authentication.SessionAuthentication')
