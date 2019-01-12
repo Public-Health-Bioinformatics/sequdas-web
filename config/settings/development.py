7ad58510568844f8469c68832caaa94b48f75fb3
Dan Fornika <dfornika@gmail.com>
2019-01-11 23:38:26 -0800

    Setting up separate dev, testing, production environments

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,3 +1,15 @@
 from .base import *
 
-MIDDLEWARE.append('config.middleware.dev_cors_middleware')
+MIDDLEWARE += [
+    'config.middleware.dev_cors_middleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    'localhost',
+]
